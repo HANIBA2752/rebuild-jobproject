0ac2fb284a50b0110934c925296084538da378b0
kritsadagorn <catsamotyba@gmail.com>
2024-12-20 08:10:57 +0700

    update midText

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import './App.css';
+import AppMidText from './components/AppItems/AppMidText';
 
 const App = () => {
   return (
-    <div className='flex items-center mt-96'>
-        <div className='web-topic ml-32 block tracking-widest'>
-            <span className='topicText topic text-6xl font-bold mb-5'>JOB EXPLORER</span>
-            <span className='topicText text-2xl'>Explore Your Future Roles</span>
-            <span className='topicText linkText'>searching for computer engineer roles &lt;&lt; HERE</span>
-        </div>
+    <div>
+      <AppMidText/>
     </div>
   );
 };
---
+++ src/components/AppItems/AppMidText.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function AppMidText() {
+  return (
+    <>
+        <div className='flex items-center mt-96'>
+            <div className='web-topic ml-32 block tracking-widest'>
+                <span className='topicText topic text-6xl font-bold mb-5'>JOB EXPLORER</span>
+                <span className='topicText text-2xl'>Explore Your Future Roles</span>
+                <span className='topicText linkText'>searching for computer engineer roles &lt;&lt; HERE</span>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default AppMidText
